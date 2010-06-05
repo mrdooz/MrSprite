30e760e198ac4926bf865fec5fe80ff89e8eff87
Magnus Osterlind <dooz@spotify.com>
2010-06-06 00:17:19 +0200

    Moved some window handling stuff from canvas to window

--- MrSprite/MainWindow.xaml.cs
+++ MrSprite/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Runtime.InteropServices;
+using System.Xml.Serialization;
 
 namespace MrSprite
 {
@@ -47,6 +48,7 @@ namespace MrSprite
             return new Point(pt.X * canvas.ActualWidth, pt.Y * canvas.ActualHeight);
         }
 
+
         private void updateCanvas()
         {
             canvas.Children.Clear();
@@ -55,12 +57,7 @@ namespace MrSprite
                 var lg = new LineGeometry() { StartPoint = _points[i].Pos, EndPoint = _points[i+1].Pos };
                 canvas.Children.Add(new Path() { Stroke = Brushes.Black, StrokeThickness = 1, Data = lg, SnapsToDevicePixels = true });
             }
-/*
-            do {
-                var lg = new LineGeometry() { StartPoint = getPoint(i - 1), EndPoint = getPoint(i) };
-                canvas.Children.Add(new Path() { Stroke = Brushes.Black, StrokeThickness = 1, Data = lg, SnapsToDevicePixels = true });
-            } while (++i < _points.Count + 1);
-            */
+
             for (int i = 0; i < _points.Count; ++i ) {
                 var cur = _points[i];
                 var rg = new RectangleGeometry() { Rect = cur.boundingRect() };
@@ -78,7 +75,7 @@ namespace MrSprite
 
             float[] mapper = new float[2 * _points.Count];
             for (int i = 0; i < _points.Count; ++i) {
-                var pt = _points[i].Pos; // getPoint(i);
+                var pt = _points[i].Pos;
                 mapper[i * 2 + 0] = (float)(pt.X / canvas.ActualWidth);
                 mapper[i * 2 + 1] = (float)(pt.Y / canvas.ActualHeight);
             }
@@ -86,20 +83,27 @@ namespace MrSprite
             var pinnedMapper = GCHandle.Alloc(mapper, GCHandleType.Pinned);
             GCHandle pinnedArray = GCHandle.Alloc(data, GCHandleType.Pinned);
             NativeMethods.create_bitmap(pinnedArray.AddrOfPinnedObject(), width, height, 32, pinnedMapper.AddrOfPinnedObject(), _points.Count * 2);
-            PixelFormat format = PixelFormats.Bgr32;
+            PixelFormat format = PixelFormats.Bgra32;
             int stride = (width * format.BitsPerPixel + 7) / 8;
-            bmpSource = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, data, stride);
+            bmpSource = BitmapSource.Create(width, height, 96, 96, format, null, data, stride);
             pinnedArray.Free();
             pinnedMapper.Free();
             img.Source = bmpSource;
         }
 
-        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private bool insideCanvas(Point pt)
+        {
+            return pt.X >= 0 && pt.X < canvas.ActualWidth && pt.Y >= 0 && pt.Y < canvas.ActualHeight;
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             moving = false;
             movingPoint = null;
 
             var pp = Mouse.GetPosition(canvas);
+            if (!insideCanvas(pp))
+                return;
 
             // don't allow multiple points on top of each other
             foreach (var p in _points) {
@@ -114,7 +118,7 @@ namespace MrSprite
             updateCanvas();
         }
 
-        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        private void Window_MouseMove(object sender, MouseEventArgs e)
         {
             if (!moving)
                 return;
@@ -131,13 +135,16 @@ namespace MrSprite
             updateCanvas();
         }
 
-        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             foreach (var p in _points)
                 p.Selected = false;
 
-            bool changed = false;
             var pp = Mouse.GetPosition(canvas);
+            if (!insideCanvas(pp))
+                return;
+
+            bool changed = false;
             foreach (var p in _points) {
                 if (p.boundingRect().Contains(pp)) {
                     p.Selected = true;
@@ -152,33 +159,6 @@ namespace MrSprite
                 moving = true;
                 startingMovePos = lastMovePos = Mouse.GetPosition(canvas);
             }
-        }
-
-
-        private void reset_Click(object sender, RoutedEventArgs e)
-        {
-            _points.Clear();
-            updateImage();
-            updateCanvas();
-        }
-
-        private void save_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.FileName = "Particle";
-            dlg.DefaultExt = ".bmp";
-            dlg.Filter = "Bmp files (.bmp)|*.bmp";
-
-            Nullable<bool> result = dlg.ShowDialog();
-
-            if (result == true) {
-                using (var s = new System.IO.FileStream(dlg.FileName, System.IO.FileMode.Create)) {
-                    var encoder = new BmpBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(bmpSource));
-                    encoder.Save(s);
-                }
-            }
-
         }
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
@@ -207,7 +187,52 @@ namespace MrSprite
             updateCanvas();
         }
 
-        class ControlPoint
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            _points.Clear();
+            updateImage();
+            updateCanvas();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Particle";
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "Png files (.png)|*.png";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true) {
+                using (var s = new System.IO.FileStream(dlg.FileName, System.IO.FileMode.Create)) {
+                    var encoder = new PngBitmapEncoder();
+                    //var conv = new FormatConvertedBitmap { Source = bmpSource, DestinationFormat = PixelFormats.Bgra32 };
+                    encoder.Frames.Add(BitmapFrame.Create(bmpSource));
+                    encoder.Save(s);
+                }
+            }
+        }
+
+        private void saveXml_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ControlPoint>));
+            System.IO.TextWriter writer = new System.IO.StreamWriter("test.xml");
+            serializer.Serialize(writer, _points);
+            writer.Close();
+        }
+
+        private void loadXml_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ControlPoint>));
+            System.IO.FileStream fs = new System.IO.FileStream("test.xml", System.IO.FileMode.Open);
+            _points = (List<ControlPoint>)serializer.Deserialize(fs);
+            fs.Close();
+            updateImage();
+            updateCanvas();
+        }
+
+        public class ControlPoint
         {
             public ControlPoint()
             {
@@ -220,21 +245,18 @@ namespace MrSprite
                 return new Rect() { X = Pos.X - rectSize, Y = Pos.Y - rectSize, Width = 2 * rectSize, Height = 2 * rectSize };
             }
 
-            public bool Selected { get; set;  }
+            [System.Xml.Serialization.XmlIgnoreAttribute]
+            public bool Selected { get; set; }
             private float rectSize = 5;
         }
-
-
-
 
         Point lastMovePos;
         Point startingMovePos;
         ControlPoint movingPoint = null;
         bool moving = false;
         BitmapSource bmpSource = null;
+
         List<ControlPoint> _points = new List<ControlPoint>();
-
-
     }
 
     internal static class NativeMethods
